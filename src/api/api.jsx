b62e5f2ce28bb2a6945e76a2a2f91d476fa24c79
koreajsm <sm705814@naver.com>
2024-05-07 11:39:25 +0900

    api키

---
+++ src/api/api.jsx
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCvVwXhNCmCaTH37M84UvJKiKR_2aqa66Q",
+    authDomain: "shopcloning-23777.firebaseapp.com",
+    projectId: "shopcloning-23777",
+    storageBucket: "shopcloning-23777.appspot.com",
+    messagingSenderId: "1073657315925",
+    appId: "1:1073657315925:web:14b880c6ffaf3f52b48adc"
+}
