0ec2ee0d1a0164fe31c324ec734f7ca834a0b9ab
koreajsm <sm705814@naver.com>
2024-05-07 11:44:39 +0900

    API KEY 수정

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,10 +1,7 @@
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCvVwXhNCmCaTH37M84UvJKiKR_2aqa66Q",
-    authDomain: "shopcloning-23777.firebaseapp.com",
-    projectId: "shopcloning-23777",
-    storageBucket: "shopcloning-23777.appspot.com",
-    messagingSenderId: "1073657315925",
-    appId: "1:1073657315925:web:14b880c6ffaf3f52b48adc"
+    apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID
 }
